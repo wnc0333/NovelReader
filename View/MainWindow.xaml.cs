c0283618e2e4717c12e71b2320ea64cff542b6ff
wnc0333 <wnc0333@163.com>
2021-08-27 13:55:01 +0800

    2021年8月27日13:53:52，右键菜单增加了自动隐蔽的选项，能够实现鼠标移开后自动隐藏，双击后自动出现的功能。

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -3,6 +3,9 @@ using System.Windows;
 using NovelReader.ViewModel;
 using System.IO;
 using System.Windows.Input;
+using System;
+using System.Windows.Controls;
+using System.Collections.Generic;
 
 namespace NovelReader.View
 {
@@ -10,10 +13,16 @@ namespace NovelReader.View
     /// MainWindow.xaml 的交互逻辑
     /// 实现接收状态更改通知接口
     /// </summary>
-    public partial class MainWindow : Window ,IStatusRecipient
+    public partial class MainWindow : Window, IStatusRecipient
     {
         // ViewModel对象
         TxtFileOpener openFile = new TxtFileOpener();
+
+        // 表示是否要进行自动隐蔽操作
+        bool autoHidden = false;
+
+        // 用来存储之前的显示面板状态
+        List<double> previousDisplayPanelState = new List<double>();
 
         public MainWindow()
         {
@@ -24,7 +33,7 @@ namespace NovelReader.View
 
             // 添加状态接收者
             openFile.AddIStatusRecipient(this);
-            
+
         }
 
         //状态通知
@@ -44,9 +53,9 @@ namespace NovelReader.View
                 + @"\小说\放开那个女巫.txt";
             //string filePath = @"G:\书\造化之王.txt";
 
-            
+
             // 读取文件
-            openFile.ReadFile(filePath,DisplayPanel);
+            openFile.ReadFile(filePath, DisplayPanel);
         }
 
         /// <summary>
@@ -60,10 +69,70 @@ namespace NovelReader.View
             e.Handled = true;
         }
 
-        //当鼠标双击时，自动隐藏窗体边框，自动将背景设为透明
+        //当鼠标双击时，自动显示
         private void DisplayPanel_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
+            // 如果处于隐蔽状态，则取消隐蔽，正常显示
+            if (autoHidden)
+            {
+                // 设置自动隐蔽模式下的显示状态的显示面板状态
+                SetVisibleDisplayPanelStateOfAutoHidden();
+            }
+        }
 
+        // 当鼠标从显示板上移开时，进入隐蔽状态
+        private void DisplayPanel_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (autoHidden)
+            {
+                // 设置自动隐蔽模式下的隐蔽状态的显示面板状态
+                SetHiddenDisplayPanelStateOfAutoHidden();
+            }
+        }
+
+        // 设置自动隐蔽模式下的隐蔽状态的显示面板状态
+        private void SetHiddenDisplayPanelStateOfAutoHidden()
+        {
+            DisplayPanel.Background.Opacity = 1;
+            DisplayPanel.Opacity = 0.01;
+        }
+
+        // 设置自动隐蔽模式下的显示状态的显示面板状态
+        private void SetVisibleDisplayPanelStateOfAutoHidden()
+        {
+            DisplayPanel.Background.Opacity = 0.01;
+            DisplayPanel.Opacity = 1;
+        }
+
+        // 当是否屏蔽的勾选框值改变时
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // 保存非隐蔽状态也就是正常状态下的显示面板状态
+            SaveNormalDisplayPanelState();
+
+            autoHidden = true;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            autoHidden = false;
+
+            RecoverNormalDisplayPanelState();
+        }
+
+        // 保存非隐蔽状态也就是正常状态下的显示面板状态
+        private void SaveNormalDisplayPanelState()
+        {
+            previousDisplayPanelState.Clear();
+            previousDisplayPanelState.Add(DisplayPanel.Opacity);
+            previousDisplayPanelState.Add(DisplayPanel.Background.Opacity);
+        } 
+        
+        // 恢复之前保存的非隐蔽状态也就是正常状态下的显示面板状态
+        private void RecoverNormalDisplayPanelState()
+        {
+            DisplayPanel.Opacity=previousDisplayPanelState[0];
+            DisplayPanel.Background.Opacity=previousDisplayPanelState[1];
         }
     }
 }
